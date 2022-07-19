c292687ff936c44b261a998aecb655b32c356c70
bababax11 <38175513+bababax11@users.noreply.github.com>
2022-07-19 17:22:53 +0900

    feat: add specification of bash

--- generate.sh
+++ generate.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 GENERATOR=typescript-axios # or typescript-fetch
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i http://back:8000/openapi.json -g ${GENERATOR} -o /local/front/src/generated --skip-validate-spec
